d7b9b03e2131f4dfed30e52fa06e22dce97fed57
Matej Capkovic <mcapkovic@gmail.com>
2021-01-17 20:46:42 +0100

    Add lowercase checkbox

---
+++ src/components/Checkbox.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let label = "";
+  export let id = Math.floor(Math.random() * 1000000 + 1);
+  export let checked = false;
+  export let disabled = false
+</script>
+
+<div class="checkbox">
+  <input class="checkbox__input" {id} type="checkbox" bind:checked disabled={disabled} /><label
+    for={id}>{label}</label
+  >
+</div>
+
+<style>
+  .checkbox {
+    display: flex;
+    align-items: center;
+  }
+  .checkbox__input {
+    margin: 0 0.3em 0 0;
+  }
+</style>
--- src/components/Typing.svelte
+++ src/components/Typing.svelte
@@ -2,6 +2,7 @@
   import Game from "./Game.svelte";
   import WPM from "./WPM.svelte";
   import CPM from "./CPM.svelte";
+  import Checkbox from "./Checkbox.svelte";
 
   import { DEFAULT_STATE } from "../constants";
   import { paragraphs } from "../text";
@@ -10,26 +11,48 @@
   let letters = [];
   let possition = -1;
   let correctWords = 0;
+  let forceLowerCase = false;
+  let words;
 
-  const words = initialText.split(" ");
-  words.forEach((word) => {
-    [...word].forEach((letter, index) => {
+  function generateContent() {
+    letters = [];
+    words.forEach((word) => {
+      [...word].forEach((letter, index) => {
+        letters.push({
+          value: letter,
+          word,
+          position: index,
+          state: DEFAULT_STATE,
+        });
+      });
       letters.push({
-        value: letter,
-        word,
-        position: index,
+        value: " ",
+        word: " ",
         state: DEFAULT_STATE,
       });
     });
-    letters.push({
-      value: " ",
-      word: " ",
-      state: DEFAULT_STATE,
-    });
-  });
+  }
+
+  $: if (forceLowerCase) {
+    words = [];
+    words = initialText.toLowerCase().split(" ");
+    generateContent();
+  } else {
+    words = [];
+    words = initialText.split(" ");
+    generateContent();
+  }
 </script>
 
 <div class="typing">
+  <div class="typing__settings">
+    <Checkbox
+      id="lowerCase"
+      label="only lower case"
+      bind:checked={forceLowerCase}
+      disabled={possition !== -1}
+    />
+  </div>
   <Game bind:letters bind:possition bind:correctWords />
   <div class="typing__stats">
     <WPM className="typing__stats__tile" {correctWords} />
@@ -46,6 +69,10 @@
   /* .typing{
     
 } */
+
+.typing__settings{
+    margin-bottom: 1em;
+}
   .typing__stats {
     display: flex;
   }
